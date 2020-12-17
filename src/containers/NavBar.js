1f715f1af105623c5d82357c6521fdf5d67afb48
giuliacajati <giuliacajati@gmail.com>
2020-12-17 16:13:56 -0500

    nav bar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,17 +1,55 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-function NavBar(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+const  LinkTab = (props) => {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
 }
 
-export default NavBar;
-// Links 
-// Login/Logout 
-// Welcome Page 
-// About 
-// Dashboard --> CourseProgress 
-// CourseIndex 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+          <LinkTab label="Welcome Page" href="/"  />
+          <LinkTab label="About" href="/"  />
+          <LinkTab label="Dashboard" href="/"  />
+          <LinkTab label="Courses" href="/"  />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
+//login logout 
+
+
