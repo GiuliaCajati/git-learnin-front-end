bf05a56fc05310201efb11a08fc4d78a9a0a248b
giuliacajati <giuliacajati@gmail.com>
2020-12-18 20:41:50 -0500

    welcome page container started

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 import About from './About.js'
 
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    container:{
+        backgroundColor: '#cfe8fc', 
+        height: '80vh',
+    },
+    welcome:{
+       marginLeft: '80vh',
+      // padding: '0px'
+    } 
+  });
+
 function Welcome(props) {
+    const classes = useStyles();
     return (
         <div>
-            <h2>WELCOME!!</h2> 
-            <h2>READY TO GIT LEARNIN??</h2>
-            <h5>MISSION, VISION AND VALUES </h5>
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="xl" className={classes.container}>
+                    <Container className={classes.welcome}>
+                        <h2>WELCOME!!</h2> 
+                        <h2>READY TO GIT LEARNIN??</h2>
+                        <h5>MISSION, VISION AND VALUES </h5>
+                    </Container>  
+                </Container>
+            </React.Fragment>
+        
             <About/>
         </div>
     );
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,55 +1,49 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import {useHistory} from 'react-router-dom'
 
-//working on link tab 
-// const  LinkTab = (props) => {
-//   return (
-//     <Tab
-//       component="a"
-//       onClick={(event) => {
-//         event.preventDefault();
-//       }}
-//       {...props}
-//     />
-//   );
-// }
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+    marginBottom: -1
+    //looking for less scrappy way
+    // marginLeft: -10,
+    // marginRight: -10,
+    // mrginTop: -10
   },
-}));
+});
 
-export default function NavBar() {
+export default function CenteredTabs() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(null);
+  const history = useHistory()
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleClick = (path) => {
+    //switch through tabs
+    history.push(path)
+  }
+
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs
-          variant="fullWidth"
-          value={value}
-          onChange={handleChange}
-          aria-label="nav tabs example"
-        >
-            <Tab label="Welcome Page" href="/welcome"/>
-            {/* <Tab label="About" href="/about"/> */}
-            <Tab label="My Dashboard" href="/dashboard"/>
-            <Tab label="Courses" href="/courses"/>
-            <Tab label="Login" href="/login"/>
-        </Tabs>
-      </AppBar>
-    </div>
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        variant="fullWidth"
+        onChange={handleChange}
+        indicatorColor="secondary"
+        textColor="secondary"
+      >
+          <Tab  label="Welcome Page" onClick={() => handleClick("/welcome")} />
+          <Tab label="My Dashboard" onClick={() => handleClick("/dashboard")} />
+          <Tab  label="Courses" onClick={() => handleClick("/courses")}  />
+          <Tab label="Login" onClick={() => handleClick("/login")}   />
+      </Tabs>
+    </Paper>
   );
 }
-//login logout 
-
-
