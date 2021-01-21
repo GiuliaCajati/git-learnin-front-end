867b2a86ce560302a8e32c57be6a5b69155ac6d2
KevJSDevelopment <Kshhn12@gmail.com>
2021-01-21 14:10:55 -0500

    code cleanup

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,31 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { render, screen, cleanup } from './test-utils'
-import userEvent from '@testing-library/user-event'
+import { render, screen, cleanup} from './test-utils'
 import Login from '../containers/Login'
-import { useSelector } from "react-redux"
+
 import { fireEvent } from '@testing-library/react'
 
 
 beforeEach(() => {
-   
-    render(<Login />)
-    
+    render(<Login />) 
 });
 
 afterEach(() => {
     cleanup()
 });
 
-test('Checking that login component renders properly', () => {
-    //before action takes care of this test. 
-})
 
-// test('Checking that login component matches snapshot', () => {
-//     const div = document.createElement('div')
-//     const component = render(<Login />, div)
-//     expect(component.container).toMatchSnapshot()
-// })
+test('Checking that login component matches snapshot', () => {
+    const div = document.createElement('div')
+    const component = render(<Login />, div)
+    expect(component.container).toMatchSnapshot()
+})
 
 test('Component has a form', () => {
     const login = screen.getByTestId('Login')
@@ -36,8 +29,6 @@ test('Submit button works', () => {
     const button = screen.getByTestId('loginSubmit')
     expect(button).toHaveAttribute('type', 'submit')
 })
-
-    
 
 test("check to see if fields can be populated", async () => {
     const usernameField = screen.getByTestId("usernameField")
